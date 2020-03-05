df93963ce0049a14a658bc2bd0589a9f0de897e6
gaspartripodi <gaspartdi@gmail.com>
2020-03-05 12:11:23 -0300

    1. Created our index page with some sections

---
+++ 01-JS-DOM-APIs/jsfile.js
@@ -0,0 +1,13 @@
+//1. Creating our index page with some sections
+
+//Self-Invoking Function
+(function () {
+  window.addEventListener("load", function (event) {
+    //When the page finished loading the 'hidden section' must fade in (attribute 'hidden' false to true)
+    const hiddenSection = document.getElementsByClassName("hidden-section")[0];
+    console.log(hiddenSection.hidden);
+    //2 seconds delay for further appreciation
+    setTimeout(() => { hiddenSection.hidden = true; }, 2000);
+    console.log(hiddenSection.hidden);
+  })
+})();
